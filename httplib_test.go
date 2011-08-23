a588efc184f5a450e3965879c7232c65159e5559
Hector Rovira <hrovira@systemsbiology.org>
2011-08-23 11:48:17 -0700

    gofmt

--- httplib_test.go
+++ httplib_test.go
@@ -5,18 +5,18 @@ import (
 )
 
 func TestFluidGet(t *testing.T) {
-        s,err := Get("http://google.com").AsString()
-        if err != nil {
-                t.Fatalf(err.String())
-        }
-        if len(s) == 0 {
-                t.Fatalf("No data available\n")
-        }
+	s, err := Get("http://google.com").AsString()
+	if err != nil {
+		t.Fatalf(err.String())
+	}
+	if len(s) == 0 {
+		t.Fatalf("No data available\n")
+	}
 }
 
 func TestInvalid(t *testing.T) {
-        _,err := Post("http://invalidurlsdfsdfasdfsdgf:9999/post").AsString()
-        if err == nil {
-            t.Fatalf("Expected an error!")
-        }
+	_, err := Post("http://invalidurlsdfsdfasdfsdgf:9999/post").AsString()
+	if err == nil {
+		t.Fatalf("Expected an error!")
+	}
 }
